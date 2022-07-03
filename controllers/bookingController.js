622cd48b8bb80bb138ea64a17fb4d71bc1737289
J.Daniel <juandaniel9619@gmail.com>
2022-07-03 00:23:35 -0400

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,50 +46,45 @@ exports.getCheckoutSession = async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({email: session.customer_email})).id
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
-  await Booking.create({tour, user, price})
-
-
-}
+  await Booking.create({ tour, user, price });
+};
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
 
-  try{
-
+  try {
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
-      );
-    }catch(err) {
-    return res.status(400).send(`Webhook error: ${err.message}`)
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if(event.type === 'checkout.session.completed'){
-    createBookingCheckout(event.data.object)
+  if (event.type === 'checkout.session.completed') {
+    createBookingCheckout(event.data.object);
   }
 
   res.status(200).json({
-    received: true
-  })
+    received: true,
+  });
 
+  // exports.createBookingCheckout = async (req, res, next) => {
+  //   try {
+  //     const { tour, user, price } = req.query;
+  //     if (!tour && !user && !price) return next();
 
-
-// exports.createBookingCheckout = async (req, res, next) => {
-//   try {
-//     const { tour, user, price } = req.query;
-//     if (!tour && !user && !price) return next();
-
-//     await Booking.create({ tour, user, price });
-//     res.redirect(req.originalUrl.split('?')[0]);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
+  //     await Booking.create({ tour, user, price });
+  //     res.redirect(req.originalUrl.split('?')[0]);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+};
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBooking = factory.getAll(Booking);
